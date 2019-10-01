f0030c63cd962f6c888e0ebd8a94c91f7f221d90
handoing <chenhandoing@gmail.com>
2019-10-02 02:47:37 +0800

    add comment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const commentDataPath = path.join(__dirname, './comment.json');
+const comment = fs.readFileSync(commentDataPath);
+
 module.exports = {
     theme: 'vuepress-theme-yubisaki',
     base: "/",
@@ -6,20 +12,36 @@ module.exports = {
     serviceWorker: true,
     evergreen: true,
     markdown: {
-        anchor: { permalink: true },
-        toc: { includeLevel: [1, 2] }
+        anchor: {
+            permalink: true
+        },
+        toc: {
+            includeLevel: [1, 2]
+        }
     },
+    comment,
     themeConfig: {
         background: `#fff`,
         github: 'handoing',
         per_page: 5,
         date_format: 'yyyy-MM-dd',
         footer: '钢铁般的回忆对你说句爱你三千遍',
-        nav: [
-            { text: '首页', link: '/' },
-            { text: '标签', link: '/tag/' },
-            { text: 'GITHUB', link: 'https://github.com/handoing' },
-            { text: '关于我', link: '/about/' },
+        nav: [{
+                text: '首页',
+                link: '/'
+            },
+            {
+                text: '标签',
+                link: '/tag/'
+            },
+            {
+                text: 'GITHUB',
+                link: 'https://github.com/handoing'
+            },
+            {
+                text: '关于我',
+                link: '/about/'
+            },
         ]
     }
 }
