7a77dbbb8d99ec0560ece1517caea2634571a2e6
handoing <chenhandoing@gmail.com>
2019-10-02 03:45:43 +0800

    update config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const commentDataPath = path.join(__dirname, './comment.json');
-const comment = fs.readFileSync(commentDataPath);
+const comment = fs.readFileSync(commentDataPath, 'utf-8');
 
 module.exports = {
     theme: 'vuepress-theme-yubisaki',
@@ -19,13 +19,20 @@ module.exports = {
             includeLevel: [1, 2]
         }
     },
-    comment,
     themeConfig: {
         background: `#fff`,
         github: 'handoing',
         per_page: 5,
         date_format: 'yyyy-MM-dd',
         footer: '钢铁般的回忆对你说句爱你三千遍',
+        comment: {
+            repo: "blog",
+            owner: "handoing",
+            locale: "zh",
+            perPage: 5,
+            autoCreateIssue: true,
+            ...JSON.parse(comment)
+        },
         nav: [{
                 text: '首页',
                 link: '/'
